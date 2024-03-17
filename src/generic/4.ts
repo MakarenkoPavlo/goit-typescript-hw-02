176fb12cb2317c929d853c68314bd7fe11e60775
Makarenko Pavlo <cmertnik1986@gmail.com>
2024-03-18 00:20:51 +0200

    finish

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,14 +2,20 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
-  constructor (public props:T) {
-
-  }
+interface ComponentProps {
+  [key: string]: any;
 }
 
-class Page extends Component {
-  pageInfo () {
+class Component<T extends ComponentProps> {
+  constructor(public props: T) {}
+}
+
+interface PageProps extends ComponentProps {
+  title: string;
+}
+
+class Page extends Component<PageProps> {
+  pageInfo() {
     console.log(this.props.title);
   }
 }
